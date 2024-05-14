b76d7696d9e6b1bb03f6d572226ac70155bae1d6
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2024-05-14 09:41:48 +0200

    add new CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import CV from "../../assets/CV Andreas Takvam 2022.pdf";
+import CV from "../../assets/CV 2.pdf";
 
 const CTA = () => {
     return (
         <div className='cta'>
-            {/* <a href={CV} download className='btn'>
-        Last Ned CV
-      </a> */}
+            <a href={CV} download className='btn'>
+                Last Ned CV
+            </a>
             <a href='#contact' className='btn'>
                 Kontakt Meg
             </a>
