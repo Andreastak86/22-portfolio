bd10bed75bb273a379ccca1f6445a48c80005a2b
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2024-05-23 19:40:02 +0200

    add long CV, but need to change layout

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import CV from "../../assets/CV 2.pdf";
+import DetailedCV from "../../assets/CV-long.pdf"; // Legg til stien til den utfyllende CVen
 
 const CTA = () => {
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownVisible(!dropdownVisible);
+    };
+
     return (
         <div className='cta'>
-            <a href={CV} download className='btn'>
-                Last Ned CV
-            </a>
+            <div className='dropdown'>
+                <button onClick={toggleDropdown} className='btn'>
+                    Last Ned CV
+                </button>
+                {dropdownVisible && (
+                    <div className='dropdown-content'>
+                        <a href={CV} download className='dropdown-item'>
+                            Last Ned CV
+                        </a>
+                        <a href={DetailedCV} download className='dropdown-item'>
+                            Last Ned Utfyllende CV
+                        </a>
+                    </div>
+                )}
+            </div>
             <a href='#contact' className='btn'>
                 Kontakt Meg
             </a>
