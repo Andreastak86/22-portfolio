ed76fc6753841586dc4627e23efb4dafa52cd2db
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2024-05-24 11:34:14 +0200

    change layout on btns

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import CV from "../../assets/CV 2.pdf";
-import DetailedCV from "../../assets/CV-long.pdf"; // Legg til stien til den utfyllende CVen
+import DetailedCV from "../../assets/CV-long.pdf";
 
 const CTA = () => {
     const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -11,20 +11,18 @@ const CTA = () => {
 
     return (
         <div className='cta'>
-            <div className='dropdown'>
-                <button onClick={toggleDropdown} className='btn'>
+            <button onClick={toggleDropdown} className='btn'>
+                Last Ned CV
+            </button>
+            <div
+                className={`dropdown-content ${dropdownVisible ? "show" : ""}`}
+            >
+                <a href={CV} download className='dropdown-item'>
                     Last Ned CV
-                </button>
-                {dropdownVisible && (
-                    <div className='dropdown-content'>
-                        <a href={CV} download className='dropdown-item'>
-                            Last Ned CV
-                        </a>
-                        <a href={DetailedCV} download className='dropdown-item'>
-                            Last Ned Utfyllende CV
-                        </a>
-                    </div>
-                )}
+                </a>
+                <a href={DetailedCV} download className='dropdown-item'>
+                    Last Ned Utfyllende CV
+                </a>
             </div>
             <a href='#contact' className='btn'>
                 Kontakt Meg
