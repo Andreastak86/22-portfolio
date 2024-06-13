678734542497242f913678d379039ee4e46e059a
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2024-06-13 14:13:45 +0200

    add functions on btn

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,26 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import CV from "../../assets/CV 2.pdf";
 import DetailedCV from "../../assets/CV-long.pdf";
 
 const CTA = () => {
     const [dropdownVisible, setDropdownVisible] = useState(false);
+    const dropdownRef = useRef(null);
 
     const toggleDropdown = () => {
         setDropdownVisible(!dropdownVisible);
     };
 
+    const handleClickOutside = (event) => {
+        if (
+            dropdownRef.current &&
+            !dropdownRef.current.contains(event.target)
+        ) {
+            setDropdownVisible(false);
+        }
+    };
+
+    const handleItemClick = () => {
+        setDropdownVisible(false);
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className='cta'>
+        <div className='cta' ref={dropdownRef}>
             <button onClick={toggleDropdown} className='btn'>
                 Last Ned CV
             </button>
             <div
                 className={`dropdown-content ${dropdownVisible ? "show" : ""}`}
             >
-                <a href={CV} download className='dropdown-item'>
+                <a
+                    href={CV}
+                    download
+                    className='dropdown-item'
+                    onClick={handleItemClick}
+                >
                     Last Ned CV
                 </a>
-                <a href={DetailedCV} download className='dropdown-item'>
+                <a
+                    href={DetailedCV}
+                    download
+                    className='dropdown-item'
+                    onClick={handleItemClick}
+                >
                     Last Ned Utfyllende CV
                 </a>
             </div>
